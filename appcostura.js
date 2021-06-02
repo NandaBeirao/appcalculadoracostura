2ec59b161c16cefe532a7df3890cf0ea62a3b333
NandaBeirao <nandabeiraose@gmail.com>
2021-06-02 00:19:04 -0300

    Update appcostura.js

--- appcostura.js
+++ appcostura.js
@@ -1,32 +1,16 @@
 const calcular = document.getElementById('calcular');
 
-function imc () {
-    const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;
+function appcostura () {
+    const diametro = document.getElementById('diametro').value;
     const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== '') {
+    if (diametro !== '') {
 
-        const valorIMC = (peso / (altura * altura)).toFixed(1);
+        const tamanhodavolta = (diametro * 3.14)).toFixed(1);
 
         let classificacao = '';
 
-        if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
-        }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
-        }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
-        }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
-        }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
-        }
-
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+        resultado.textContent = `${nome} corte ${tamanhodavolta}`;
         
     }else {
         resultado.textContent = 'Preencha todos os campos!!!';
@@ -34,4 +18,4 @@ function imc () {
 
 }
 
-calcular.addEventListener('click', imc);
+calcular.addEventListener('click', appcostura);
