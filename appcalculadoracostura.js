7daf743798ce17351ec2a1831e23cd66b0d99bdf
NandaBeirao <nandabeiraose@gmail.com>
2021-06-02 00:49:37 -0300

    Update appcalculadoracostura.js

--- appcalculadoracostura.js
+++ appcalculadoracostura.js
@@ -2,11 +2,8 @@ const calcular = document.getElementById('calcular');
 
 function appcalculadoracostura () {
     const diametro = document.getElementById('diametro').value;
-    const resultado = document.getElementById('resultado');
-
-    if (diametro !== '') {
-
-        const appcalculadoracostura = (diametro * 3.14).toFixed(1);
+    const resultado = document.getElementById('resultado')
+    const appcalculadoracostura = (diametro * 3.14).toFixed(1);
 }
 
 calcular.addEventListener('click', appcalculadoracostura);
