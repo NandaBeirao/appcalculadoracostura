70a700291a6604445b66746f980976c1c7e2270e
NandaBeirao <nandabeiraose@gmail.com>
2021-06-02 15:10:09 -0300

    Update appcalculadoracostura.js

--- appcalculadoracostura.js
+++ appcalculadoracostura.js
@@ -1,7 +1,24 @@
 const calcular = document.getElementById('calcular');
 
-function appcalculadoracostura {
-  var raio = document.getElementById('raio').value;  
-  var appcalculadoracostura = 2 * Math.PI * raio;}
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
 
-calcular.addEventListener('onclick', appcalculadoracostura);
+    if (nome !== '' && peso !== '') {
+
+        const valorIMC = (peso * Math.PI).toFixed(1);
+
+        let classificacao = '';
+
+ 
+
+        resultado.textContent = `${nome} corte ${valorIMC} cm de pano para dar a volta no círculo`;
+        
+    }else {
+        resultado.textContent = 'Preencha todos os campos!!!';
+    }
+
+}
+
+calcular.addEventListener('click', imc);
