2dd4c5826def1b0a97ab6a044dfd3ca000c60029
NandaBeirao <nandabeiraose@gmail.com>
2021-06-02 02:15:56 -0300

    Update appcalculadoracostura.js

--- appcalculadoracostura.js
+++ appcalculadoracostura.js
@@ -2,6 +2,6 @@ const calcular = document.getElementById('calcular');
 
 function appcalculadoracostura {
   var raio = document.getElementById('raio').value;  
-  var circumference = 2 * Math.PI * raio;}
+  var appcalculadoracostura = 2 * Math.PI * raio;}
 
 calcular.addEventListener('onclick', appcalculadoracostura);
